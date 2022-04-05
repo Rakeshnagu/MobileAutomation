ba3cbbaffe2c3de658a3f5ad2d03746ffc2dbcfc
Rakeshnagu <r.nagulapally@getinkbox.com>
2022-04-05 11:45:14 -0400

    Created Framework for Mobile automation

---
+++ first/src/test/java/test/BrowseSample.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class BrowseSample extends AutomationChromeBase {
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		// 1
+		//AndroidDriver<AndroidElement> driver =AutomationChromeBase.setupCapabilities();
+		
+		// 2
+		//driver.get("http://facebook.com");
+    
+		// 3
+		//.findElementByXPath("//*[@id='m_login_email']").sendKeys("your_email@gmail.com");
+		//.findElementByXPath("//*[@id='m_login_password']").sendKeys("your_password");
+		//driver.findElementByXPath("//*[@id=\"u_0_6\"]").click();	
+	}
+}
