5fdc83d9d68e371ce8099836a22faad4b5225e12
Rakeshnagu <100619204+Rakeshnagu@users.noreply.github.com>
2022-05-11 23:43:54 -0400

    adding testcase related to footer - C21942_Validate_Customer_Care_InFooter --fixed failure - C21945_Validate_BottomFooter_TextLogo - C21948_Terms_of_Service_and_Privacy_Policy - C21940_Validate_EmailBox_InFooter

--- src/test/java/inkbox/testcase/FooterTest.java
+++ src/test/java/inkbox/testcase/FooterTest.java
@@ -4,15 +4,16 @@ import inkbox.wrapper.BaseClass;
 import inkbox.wrapper.RetryListener;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class FooterTest extends BaseClass {
 
-    @Test(retryAnalyzer= RetryListener.class, enabled = true, description = "Validate Bold Text of footer with 'Excellent' Star Reviews and Trustpilot text")
-    public void C21937_ValidateBoldTextFooter_ExcellentStarTrustPilot(){
-        Assert.assertTrue(footer.checkPoweredByExist(),"Bold Text of footer with 'Excellent' Star Reviews and Trustpilot text not present");
+    @Test(retryAnalyzer = RetryListener.class, enabled = true, description = "Validate Bold Text of footer with 'Excellent' Star Reviews and Trustpilot text")
+    public void C21937_ValidateBoldTextFooter_ExcellentStarTrustPilot() {
+        Assert.assertTrue(footer.checkPoweredByExist(), "Bold Text of footer with 'Excellent' Star Reviews and Trustpilot text not present");
     }
 
-    @Test(retryAnalyzer= RetryListener.class, enabled = true, description = "Validate about Features under the \"About INKBOX\" whether they navigate to required page or not")
+    @Test(retryAnalyzer = RetryListener.class, enabled = true, description = "Validate about Features under the \"About INKBOX\" whether they navigate to required page or not")
     public void Validate_About_INKBOX_InFooter() {
         footer.Validate_About_Linkbox_Footer();
     }
@@ -26,4 +27,27 @@ public class FooterTest extends BaseClass {
     public void C21942_Validate_Customer_Care_InFooter() {
         footer.Validate_Customer_Care_Footer();
     }
+
+    @Test(retryAnalyzer = RetryListener.class, description = "Validate bottom of Footer Text Logo 'INKBOX' year of Copyrights ,Proudly Made in North America ,Currency , Terms of Service and Privacy Policy")
+    public void C21945_Validate_BottomFooter_TextLogo() {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(footer.checkLogoExist(), "logo is not present in the footer");
+        softAssert.assertTrue(footer.checkCopyrightTextExist(), " Copyright © 2022 inkbox ink inc. Proudly Made In North America -- text is missing on footer");
+        softAssert.assertTrue(footer.checkPrivacyPolicyExist(), "privacy policy text missing");
+        softAssert.assertTrue(footer.checkTermsOfServiceExist(), "terms of service text missing");
+        softAssert.assertAll();
+    }
+
+    @Test(retryAnalyzer = RetryListener.class, description = "validate and click on Terms of Service and Privacy Policy navigate to their respective page")
+    public void C21948_Terms_of_Service_and_Privacy_Policy() {
+        footer.clickTermsOfService();
+        footer.clickPrivacyPolicyText();
+    }
+
+    @Test(retryAnalyzer = RetryListener.class, description = "Validate the email box by giving credentials to subscription")
+    public void C21940_Validate_EmailBox_InFooter() throws InterruptedException {
+        footer.validate_EmailField_Footer();
+    }
+
+
 }
